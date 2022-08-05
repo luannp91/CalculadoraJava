3ff168c47c2c79f7e70c538f93bc1d8b724b90b4
Luan Pinto <luan.nelmo@hotmail.com>
2022-08-05 18:34:29 -0300

    Componente Display

---
+++ src/com/luannp/calc/visao/Botao.java
@@ -0,0 +1,6 @@
+package com.luannp.calc.visao;
+
+import javax.swing.*;
+
+public class Botao extends JButton {
+}
---
+++ src/com/luannp/calc/visao/Teclado.java
@@ -0,0 +1,11 @@
+package com.luannp.calc.visao;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Teclado extends JPanel {
+
+    public Teclado() {
+        setBackground(Color.BLACK);
+    }
+}
