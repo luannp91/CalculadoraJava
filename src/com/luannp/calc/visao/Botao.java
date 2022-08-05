45e5cd705707cc65b6844d689d4036f7161f9b56
Luan Pinto <luan.nelmo@hotmail.com>
2022-08-05 18:41:47 -0300

    Componente Botao

--- src/com/luannp/calc/visao/Botao.java
+++ src/com/luannp/calc/visao/Botao.java
@@ -1,6 +1,16 @@
 package com.luannp.calc.visao;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Botao extends JButton {
+
+    public Botao(String texto, Color cor) {
+        setText(texto);
+        setOpaque(true);
+        setBackground(cor);
+        setForeground(Color.WHITE);
+        setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        setFont(new Font("courier", Font.PLAIN, 25));
+    }
 }
